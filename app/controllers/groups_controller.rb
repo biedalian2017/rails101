afb38e8c573245e897f020a41b2ae1709e8098d4
李阳 <admin@admindeMacBook-Air.local>
2017-02-26 21:45:32 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,8 +7,9 @@ class GroupsController < ApplicationController
   end
 
   def show
-   @group = Group.find(params[:id])
- end
+    @group = Group.find(params[:id])
+    @posts = @group.posts
+  end
 
  def edit
   end
@@ -29,7 +30,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    
+
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
